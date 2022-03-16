9a8259530f57e86c234d43e4b7dd092bdc7af9dd
Samuel F. Potter <sfp@cims.nyu.edu>
2022-03-16 17:31:10 -0400

    add bfToc

--- util.c
+++ util.c
@@ -1,5 +1,14 @@
 #include "util.h"
 
+#include <time.h>
+
+BfReal bfToc() {
+  static clock_t t1 = 0;
+  clock_t t0 = t1;
+  t1 = clock();
+  return ((double)t1 - (double)t0)/CLOCKS_PER_SEC;
+}
+
 void bfSizeSetConstant(BfSize numSizes, BfSize *size, BfSize value) {
   for (BfSize i = 0; i < numSizes; ++i)
     size[i] = value;
--- util.h
+++ util.h
@@ -2,5 +2,7 @@
 
 #include "def.h"
 
+BfReal bfToc();
+
 void bfSizeSetConstant(BfSize numSizes, BfSize *size, BfSize value);
 void bfSizeRunningSum(BfSize numSizes, BfSize *size);
