98a6d9b5d469c19924f6609b9733fa39efe56f44
Samuel F. Potter <sfp@cims.nyu.edu>
2022-03-17 12:44:55 -0400

    pass number of points as CLI arg

--- make_test_data.py
+++ make_test_data.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 
 import numpy as np
+import sys
 
-theta = np.linspace(0, 2*np.pi, 2**14, endpoint=False)
+if len(sys.argv) == 1:
+    print(f'usage: {sys.argv[0]} <num_points>')
+    exit()
+
+num_points = int(sys.argv[1])
+
+theta = np.linspace(0, 2*np.pi, num_points, endpoint=False)
 
 points = np.array([np.cos(theta), np.sin(theta)]).T
 
