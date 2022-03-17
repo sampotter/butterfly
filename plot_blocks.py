d9e418adca9c482c8beebc9efba7dc5a0b55a0cb
Samuel F. Potter <sfp@cims.nyu.edu>
2022-03-17 17:57:37 -0400

    looking a little better...

--- plot_blocks.py
+++ plot_blocks.py
@@ -47,22 +47,21 @@ plt.show()
 # now make a nicer block matrix plot
 
 type2hatch = {
-    5: None,
-    7: '**',
+    5: '//',
+    7: 'o',
 }
 
-cmap = cc.cm.colorwheel
+cmap = cc.cm.gouldian
 
 fig, ax = plt.subplots()
 for level, i0, i1, j0, j1, type_ in zip(Level, I0, I1, J0, J1, Type):
     di, dj = i1 - i0, j1 - j0
     fc = cmap(level/(max_level + 1))
-    ec = cmap((level - 1)/(max_level + 1))
+    ec = cmap((level - 0.5)/(max_level + 1))
     h = type2hatch[type_]
-    rect = Rectangle((j0, i0), dj, di, linewidth=0.5, facecolor=fc, edgecolor=ec, hatch=h)
+    rect = Rectangle((j0, i0), dj, di, linewidth=1, facecolor=fc, edgecolor=ec, hatch=h)
     ax.add_patch(rect)
 ax.set_aspect('equal')
-ax.invert_yaxis()
 ax.set_xlim(0, num_cols - 1)
-ax.set_ylim(0, num_rows - 1)
+ax.set_ylim(num_rows - 1, 0)
 plt.show()
