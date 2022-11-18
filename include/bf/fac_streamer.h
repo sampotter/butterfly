276b8cac712f1c32bb1538d96717bb90f6ee5807
Samuel F. Potter <sfp@cims.nyu.edu>
2022-11-18 13:19:10 -0500

    update fac_streamer.h

--- include/bf/fac_streamer.h
+++ include/bf/fac_streamer.h
@@ -1,15 +1,15 @@
 #pragma once
 
+#include "mat.h"
 #include "tree.h"
-
-typedef enum BfStreamModes { BF_STREAM_MODE_POST_ORDER } BfStreamMode;
 
 typedef struct BfFacStreamer BfFacStreamer;
 
 BfFacStreamer *bfFacStreamerNew();
-void bfFacStreamerInit(BfFacStreamer *facStreamer, BfTree const *rowTree,
-                       BfTree const *colTree, BfStreamingMode mode,
+void bfFacStreamerInit(BfFacStreamer *facStreamer,
+                       BfTree const *rowTree, BfTree const *colTree,
                        BfSize rowTreeInitDepth, BfSize colTreeInitDepth,
                        BfReal tol);
 void bfFacStreamerFeed(BfFacStreamer *facStreamer, BfMat const *mat);
 bool bfFacStreamerDone(BfFacStreamer const *facStreamer);
+BfMat *bfFacStreamerGetFac(BfFacStreamer const *facStreamer);
