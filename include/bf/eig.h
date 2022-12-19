6219569f1300f4a29c83a7cd579d3a81490ea7d2
Samuel F. Potter <sfp@cims.nyu.edu>
2022-12-19 14:32:59 -0500

    style for C++ files

--- include/bf/eig.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "mat.h"
-
-BfReal bfGetEigMax(BfMat const *L, BfMat const *M);
-void bfGetEigBand(BfMat const *L, BfMat const *M, BfReal lam0, BfReal lam1,
-                  BfMat **Phi, BfMat **Lam);
--- include/bf/spmat.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "def.h"
-
-typedef struct BfSpmat BfSpmat;
-
-/* Upcasting: */
-
-BfMat *bfSpmatToMat(BfSpmat *spmat);
