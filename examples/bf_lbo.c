393df90343d2437c919bb7278cfc3e944dd995da
Samuel F. Potter <sfp@cims.nyu.edu>
2023-03-13 13:57:37 -0400

    simplify this for now

--- examples/bf_lbo.c
+++ examples/bf_lbo.c
@@ -225,22 +225,11 @@ int main(int argc, char const *argv[]) {
   while (!bfFacStreamerIsDone(facStreamer)) {
     feedFacStreamerNextEigenband(facStreamer, freqs, L, M);
     HANDLE_ERROR();
-
-    // TODO: for testing, should be able to stop here, extract what I
-    // have so far, and check that I have a correct factorization of
-    // the columns that I've streamed
   }
-
-  BfMat *fac = bfFacStreamerGetFac(facStreamer);
-  HANDLE_ERROR();
-
-  /* Do numerical tests */
-
-  /* Clean up */
 
   END_ERROR_HANDLING() {}
 
-  bfMatDelete(&fac);
+  /* Clean up */
   bfTreeDelete(&rowTree);
   bfOctreeDeinit(&octree);
   bfMatDelete(&M);
