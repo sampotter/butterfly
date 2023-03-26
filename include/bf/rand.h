27e562efd9056df230b044aa051405dcd1fc2f4e
Samuel F. Potter <sfp@cims.nyu.edu>
2023-03-26 13:52:25 -0400

    add new functions in rand.h

--- include/bf/rand.h
+++ include/bf/rand.h
@@ -1,10 +1,17 @@
 #pragma once
 
 #include "def.h"
+#include "geom.h"
 
 void bfSeed(BfSize seed);
 
+BfSize bfSizeUniform1(BfSize low, BfSize high);
+
+BfReal bfRealUniform1();
 void bfRealUniform(BfSize n, BfReal *x);
+
 void bfRealRandn(BfSize n, BfReal *x);
 
 void bfComplexRandn(BfSize n, BfComplex *x);
+
+void bfSampleRandomUnitVector2(BfVector2 u);
