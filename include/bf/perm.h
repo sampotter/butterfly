06927412dce02b43ec0de61bcb5fb5e40d8e59e3
Samuel F. Potter <sfp@cims.nyu.edu>
2023-04-07 17:06:04 -0400

    updating BfPerm...

--- include/bf/perm.h
+++ include/bf/perm.h
@@ -13,5 +13,7 @@ void bfPermInitEmpty(BfPerm *perm, BfSize size);
 void bfPermDeinit(BfPerm *perm);
 void bfPermDealloc(BfPerm **perm);
 void bfPermDelete(BfPerm **perm);
+BfPerm *bfPermCopy(BfPerm const *perm);
 BfPerm bfPermIdentity(BfSize size);
 BfPerm bfPermGetReversePerm(BfPerm const *perm);
+BfSize bfPermGetSize(BfPerm const *perm);
