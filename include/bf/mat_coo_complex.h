9db9b671a348280e3669d056c17071666d07490d
Samuel F. Potter <sfp@cims.nyu.edu>
2023-04-07 17:08:07 -0400

    make BfMatCooComplex resizable and implement its BfMatAddInplace

--- include/bf/mat_coo_complex.h
+++ include/bf/mat_coo_complex.h
@@ -12,15 +12,21 @@ BfMat *bfMatCooComplexGetRowRangeCopy(BfMat const *mat, BfSize i0, BfSize i1);
 BfMat *bfMatCooComplexGetColRangeCopy(BfMat const *mat, BfSize j0, BfSize j1);
 void bfMatCooComplexPermuteRows(BfMat *mat, BfPerm const *perm);
 void bfMatCooComplexPermuteCols(BfMat *mat, BfPerm const *perm);
+void bfMatCooComplexAddInplace(BfMatCooComplex *matCooComplex, BfMat const *otherMat);
 BfMat *bfMatCooComplexMul(BfMat const *mat, BfMat const *otherMat);
 bool bfMatCooComplexIsZero(BfMat const *mat);
 
 struct BfMatCooComplex {
   BfMat super;
+
   BfSize numElts;
   BfSize *rowInd;
   BfSize *colInd;
   BfComplex *value;
+
+  /* This capacity does triple-duty as the capacity for the memory
+   * backing `rowInd`, `colInd`, and `value`. */
+  BfSize capacity;
 };
 
 /* Upcasting: */
