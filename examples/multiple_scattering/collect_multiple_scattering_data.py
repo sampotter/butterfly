0344968fa4044ffe9643fa252f622fc9d2c3a90d
Samuel F. Potter <sfp@cims.nyu.edu>
2023-04-21 15:12:09 -0400

    small improvements to collect_multiple_scattering_data.py

--- examples/multiple_scattering/collect_multiple_scattering_data.py
+++ examples/multiple_scattering/collect_multiple_scattering_data.py
@@ -8,23 +8,24 @@ import time
 from pathlib import Path
 
 MIN_POINTS = 10
+MAX_POINTS = 250000
 
-# The K grid for tests. We set up several uniforms grids at different
-# scales. The first one from k = 1 to 9, the second from k = 10 to 90,
-# and the final one from k = 100 to 1000 (with the obvious spacing).
-K = np.concatenate([
-    np.linspace(1.0, 9.0, 9),
-    np.linspace(10.0, 90.0, 9),
-    np.linspace(100.0, 1000.0, 10)
-])
+K = np.logspace(0, 3, 13)
 
 # The H grid for tests. We set it up to match K so that each value
 # will give roughly 10 points per wavelength for that wavenumber.
-H = 2*np.pi/(MIN_POINTS*K)
+# H = 2*np.pi/(MIN_POINTS*K)
 
-R = np.array([0.025, 0.05,  0.1,  0.2,  0.3,  0.4, 0.5])[::-1]
-A = np.array([0.005, 0.01, 0.02, 0.04, 0.06, 0.08, 0.1])[::-1]
-B = np.array([ 0.01, 0.02, 0.04, 0.08, 0.12, 0.16, 0.2])[::-1]
+def get_H():
+    k = 0
+    while True:
+        h = (2*np.pi/MIN_POINTS)*10**(-k/4)
+        yield h
+        k += 1
+
+R = np.array([ 0.1,  0.2,  0.3,  0.4, 0.5])[::-1]
+A = R/5
+B = 2*A
 
 def get_lambda(k):
     return 2*np.pi/k
@@ -42,28 +43,46 @@ if __name__ == '__main__':
     results_raw_dir_path = results_dir_path/'raw'
     results_raw_dir_path.mkdir(exist_ok=True)
 
-    for k, (r, a, b), h in it.product(K, zip(R, A, B), H):
-        ppw = get_points_per_wavelength(k, h)
-        ppe = get_points_per_ellipse(a, b, h)
-        if ppw < MIN_POINTS or ppe < MIN_POINTS:
-            continue
+    for k, (r, a, b) in it.product(K, zip(R, A, B)):
+        for h in get_H():
+            ppw = get_points_per_wavelength(k, h)
+            ppe = get_points_per_ellipse(a, b, h)
+            if ppw < MIN_POINTS or ppe < MIN_POINTS:
+                continue
 
-        output_path = results_raw_dir_path/f'k{k}_r{r}_a{a}_b{b}_h{h:0.1E}.txt'
-        if output_path.exists():
-            continue
+            output_path = results_raw_dir_path/f'k{k}_r{r}_a{a}_b{b}_h{h:0.1E}.txt'
+            if output_path.exists():
+                continue
 
-        print(f'{k=}, {r=}, {a=}, {b=}, {h=:0.1E}, {ppw=:0.1f}, {ppe=:0.1f}...', end='', flush=True)
+            # set up the discretization (basically free) and check how
+            # many points it has---if there are too many, we break
+            # from the loop here and move on
+            #
+            # basically, we will continue generating smaller and
+            # smaller h until we hit the maximum number of DOFs
+            cmd = ['./multiple_scattering', f'-k{k}', f'-r{r}', f'-a{a}', f'-b{b}', f'-h{h}']
+            proc = subprocess.run(cmd, capture_output=True)
+            lines = proc.stdout.decode('ascii').split('\n')
+            line = next(_ for _ in lines if 'Setting up discretization...' in _)
+            num_points = int(line.split()[-3])
+            if num_points > MAX_POINTS:
+                break
 
-        t0 = time.time()
+            print(f'{k=}, {r=}, {a=}, {b=}, {h=:0.1E}:')
+            print(f'- {ppw=:0.1f}')
+            print(f'- {ppe=:0.1f}')
+            print(f'- {num_points=}')
 
-        cmd = ['./multiple_scattering', f'-k{k}', f'-r{r}', f'-a{a}', f'-b{b}', f'-h{h}',
-               '--doButterflyPreconditionedGmres', '--doFmmPreconditionedGmres']
+            t0 = time.time()
 
-        proc = subprocess.run(cmd, capture_output=True)
+            cmd = ['./multiple_scattering', f'-k{k}', f'-r{r}', f'-a{a}', f'-b{b}', f'-h{h}',
+                   '--doButterflyPreconditionedGmres', '--doFmmPreconditionedGmres']
 
-        tf = time.time()
+            proc = subprocess.run(cmd, capture_output=True)
 
-        print(f' done [{tf - t0:.02f}s]')
+            tf = time.time()
 
-        with open(output_path, 'w') as f:
-            print(proc.stdout.decode('ascii'), file=f)
+            print(f'- finished: elapsed time = [{tf - t0:.02f}s]')
+
+            with open(output_path, 'w') as f:
+                print(proc.stdout.decode('ascii'), file=f)
