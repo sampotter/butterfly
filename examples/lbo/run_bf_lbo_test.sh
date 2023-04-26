68d11dd470ed30203cca9f2d1bdd69834b6f9c5c
Samuel F. Potter <sfp@cims.nyu.edu>
2023-04-26 10:10:37 -0400

    fix bug in run_bf_lbo_test.sh

--- examples/lbo/run_bf_lbo_test.sh
+++ examples/lbo/run_bf_lbo_test.sh
@@ -2,8 +2,9 @@
 
 OBJ_PATH=dragon0.obj
 TOL=1e-3
+P=0.0625
 SPACE_TREE_OFFSET=0
-FREQ_TREE_OFFSET=0
+FREQ_TREE_OFFSET=5
 
 echo "running bf_lbo example:"
 echo "- obj file: $OBJ_PATH"
@@ -13,6 +14,6 @@ echo "- freq tree offset: $FREQ_TREE_OFFSET"
 
 set -x
 
-./bf_lbo $OBJ_PATH $TOL $SPACE_TREE_OFFSET $FREQ_TREE_OFFSET
+./bf_lbo $OBJ_PATH $TOL $P $SPACE_TREE_OFFSET $FREQ_TREE_OFFSET
 
 set +x
