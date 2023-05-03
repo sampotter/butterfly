346bf352e81efaabeb928d2422da59c8e0d82666
Samuel F. Potter <sfp@cims.nyu.edu>
2023-05-03 16:30:18 -0400

    adding BfPolicy enum

--- include/bf/def.h
+++ include/bf/def.h
@@ -50,3 +50,9 @@ static BfSize const BF_DEFAULT_STRIDE = 1;
 static BfSize const BF_ARRAY_DEFAULT_CAPACITY = 128;
 
 typedef int (*BfCompar)(const void *, const void *, void *);
+
+typedef enum BfPolicy {
+  BF_POLICY_VIEW,
+  BF_POLICY_COPY,
+  BF_POLICY_STEAL
+} BfPolicy;
