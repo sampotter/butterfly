24ff6f9d5163e9e781bfa5e5d43f290bf3af0dcd
Samuel F. Potter <sfp@cims.nyu.edu>
2023-06-15 14:40:35 -0400

    add SORT2 macro

--- src/macros.h
+++ src/macros.h
@@ -5,3 +5,9 @@
     x = y;                                      \
     y = tmp;                                    \
   } while (0)
+
+#define SORT2(x, y) do {                        \
+    __typeof__(x) tmp = x < y ? x : y;          \
+    y = x < y ? y : x;                          \
+    x = tmp;                                    \
+  } while (0)
