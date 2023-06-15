6de66f035c5d3d2e396eeca9ba585e484ad2d621
Samuel F. Potter <sfp@cims.nyu.edu>
2023-06-15 14:38:49 -0400

    provide bfGetShiftedEigs declaration in linalg.h

--- include/bf/linalg.h
+++ include/bf/linalg.h
@@ -15,5 +15,6 @@ BfSize bfTruncSpecGetNumTerms(BfTruncSpec const *truncSpec, BfMatDiagReal const 
 
 BfMat *bfSolveGMRES(BfMat const *A, BfMat const *B, BfMat *X0, BfReal tol, BfSize maxNumIter, BfSize *numIter, BfMat const *M);
 BfReal bfGetMaxEigenvalue(BfMat const *L, BfMat const *M);
+void bfGetShiftedEigs(BfMat const *A, BfMat const *M, BfReal sigma, BfSize k, BfMat **PhiPtr, BfVecReal **LambdaPtr);
 void bfGetEigenband(BfMat const *A, BfMat const *M, BfReal lam0, BfReal lam1, BfReal sigma, BfMat **PhiPtr, BfVecReal **LambdaPtr);
 bool bfGetTruncatedSvd(BfMat const *mat, BfMat **U, BfMatDiagReal **S, BfMat **V, BfTruncSpec const *truncSpec, BfBackend backend);
