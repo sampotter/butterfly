cb90d3b8fa1a7ef32abe9f1b17b266d16b70113a
Samuel F. Potter <sfp@cims.nyu.edu>
2023-06-19 12:07:41 -0400

    print stats

--- examples/fiedler_tree/test_fiedler_tree.c
+++ examples/fiedler_tree/test_fiedler_tree.c
@@ -1,6 +1,7 @@
 #include <bf/assert.h>
 #include <bf/error.h>
 #include <bf/fiedler_tree.h>
+#include <bf/util.h>
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
@@ -10,12 +11,18 @@ int main(int argc, char *argv[]) {
 
   char const *objPath = argv[1];
 
-  BfTrimesh *trimesh = bfTrimeshNewFromObjFile(objPath);
-
   BfReal tol = 1e-15;
 
-  printf("\nbuilding Fielder tree:\n");
+  bfToc();
+
+  BfTrimesh *trimesh = bfTrimeshNewFromObjFile(objPath);
+
+  printf("loaded triangle mesh (%lu verts and %lu faces) [%.1fs]\n",
+         bfTrimeshGetNumVerts(trimesh), bfTrimeshGetNumFaces(trimesh), bfToc());
+
   BfFiedlerTree *fiedlerTree = bfFiedlerTreeNewFromTrimesh(trimesh, tol, /* keepNodeTrimeshes: */ true);
+
+  printf("built Fiedler tree [%.1fs]\n", bfToc());
 
   bfFiedlerTreeDeinitAndDealloc(&fiedlerTree);
   bfTrimeshDeinitAndDealloc(&trimesh);
