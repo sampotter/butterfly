1a0d75f8f3ee24bc59bfc6ac1ad5cb13975c1ace
Samuel F. Potter <sfp@cims.nyu.edu>
2023-06-19 10:49:23 -0400

    fixing indexing

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,10 @@
-;;; Directory Local Variables
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((c-mode . ((indent-tabs-mode . nil)
-			(c-basic-offset . 2)
-			(comment-style . 'multi-line)))
- (c++-mode . ((indent-tabs-mode . nil)
+((c++-mode . ((indent-tabs-mode . nil)
 			  (c-basic-offset . 2)
-			  (comment-style . 'multi-line))))
+			  (comment-style . multi-line)))
+ (c-mode . ((indent-tabs-mode . nil)
+			(c-basic-offset . 2)
+			(comment-style . multi-line)))
+ (nil . ((projectile-indexing-method . native))))
