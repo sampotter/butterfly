9654c31922a234ff8d5f784552c6a4bc8c185881
Samuel F. Potter <sfp@cims.nyu.edu>
2023-08-08 16:50:31 -0700

    update .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,10 +1,28 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((c++-mode . ((indent-tabs-mode . nil)
+((nil . ((eval . (define-skeleton
+				   bf-error-skeleton
+				   "Inserts an error handling section delimited by
+ calls to BF_ERROR_BEGIN and BF_ERROR_END."
+				   nil
+				   \n > "BF_ERROR_BEGIN();"
+				   \n >
+				   \n > _
+				   \n >
+				   \n > "BF_ERROR_END() {"
+				   \n > "BF_DIE();"
+				   \n > "}" >))
+		 (projectile-indexing-method . native)))
+ (c++-mode . ((indent-tabs-mode . nil)
 			  (c-basic-offset . 2)
 			  (comment-style . multi-line)))
  (c-mode . ((indent-tabs-mode . nil)
 			(c-basic-offset . 2)
-			(comment-style . multi-line)))
- (nil . ((projectile-indexing-method . native))))
+			(comment-style . multi-line)
+			(visual-line-mode . t)
+			(visual-fill-column-mode . nil)
+			(adaptive-wrap-prefix-mode . t)
+			(adaptive-wrap-extra-indent . 4)))
+ (python-mode . ((subword-mode)))
+ (cython-mode . ((subword-mode))))
