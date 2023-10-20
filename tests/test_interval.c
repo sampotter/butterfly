5424a17418a495997ffa41854587de9a49be70d7
Samuel F. Potter <sfp@cims.nyu.edu>
2023-10-19 20:56:52 -0700

    flesh out test_interval_difference

--- tests/test_interval.c
+++ tests/test_interval.c
@@ -5,6 +5,8 @@
 static void test_interval_difference(void **state) {
   (void)state;
 
+  /* Expected behavior: [-1, 2] \ (0, 1) = [-1, 0] U [1, 2]. */
+
   BfInterval I1 = {.endpoint = {-1, 2}, .closed = {true, true}};
   BfInterval I2 = {.endpoint = {0, 1}, .closed = {false, false}};
 
@@ -12,6 +14,14 @@ static void test_interval_difference(void **state) {
   BfSize n = bfIntervalDifference(&I1, &I2, diff);
 
   assert_int_equal(n, 2);
+  assert_true(diff[0].endpoint[0] == -1);
+  assert_true(diff[0].endpoint[1] == 0);
+  assert_true(diff[0].closed[0]);
+  assert_true(diff[0].closed[1]);
+  assert_true(diff[1].endpoint[0] == 1);
+  assert_true(diff[1].endpoint[1] == 2);
+  assert_true(diff[1].closed[0]);
+  assert_true(diff[1].closed[1]);
 }
 
 int main(void) {
