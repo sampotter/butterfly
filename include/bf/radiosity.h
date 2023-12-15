3ca10ce554086002156ba253a9df8cdbe2c0f849
Samuel F. Potter <sfp@cims.nyu.edu>
2023-12-14 22:56:45 -0800

    don't want radiosity.{c,h}

--- include/bf/radiosity.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "geom.h"
-#include "types.h"
-
-BfMatCsrReal *bfRadiosityGetViewFactorMatrix(BfTrimesh const *trimesh, BfSizeArray const *rowInds, BfSizeArray const *colInds);
